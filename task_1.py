6c1dcec6b85a375c312edd756e0599bcac9655fa
e.zhydzetski <e.zhydzetski@gmail.com>
2017-09-12 16:44:29 +0300

    add Point and PolygonFactory

--- task_1.py
+++ task_1.py
@@ -3,34 +3,55 @@ import math
 import random
 
 
-class Polygon:
-    def __init__(self, center_x, center_y, radius, vertex_count):
-        self.__vertex_count = vertex_count
-        self.__xs = []
-        self.__ys = []
-        seg = math.pi * 2 / vertex_count / 10
-        for i in range(0, self.__vertex_count):
-            rand = random.randint(0, 9)
-            dir = seg * (10 * i + rand)
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-            self.__xs.append(center_x + radius * math.cos(dir))
-            self.__ys.append(center_y + radius * math.sin(dir))
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
+
+class Polygon:
+    def __init__(self, vertex_points):
+        self.vertex_points = vertex_points
 
     def paint(self, canvas):
-        vertexes = []
-        for i in range(0, self.__vertex_count):
-            vertexes.append(self.__xs[i])
-            vertexes.append(self.__ys[i])
-        canvas.create_polygon(vertexes, fill='white', outline="red")
+        paintable_vertexes = []
+        for vertex_point in self.vertex_points:
+            paintable_vertexes.append(vertex_point.x)
+            paintable_vertexes.append(vertex_point.y)
+        canvas.create_polygon(paintable_vertexes, fill='white', outline="red")
+
+
+class PolygonFactory:
+    @staticmethod
+    def create_random(center_x, center_y, radius, vertex_count):
+        vertex_points = []
+        segment = math.pi * 2 / vertex_count / 10
+        for i in range(0, vertex_count):
+            rand = random.randint(0, 9)
+            direction = segment * (10 * i + rand)
+
+            vertex_points.append(Point(
+                center_x + radius * math.cos(direction),
+                center_y + radius * math.sin(direction))
+            )
+        return Polygon(vertex_points)
+
+
+class Route:
+    def __init__(self, start, finish, step_count):
+        self.steps = []
 
 
 c = Canvas(width=800, height=600, bg='grey80')
 c.pack()
 
-p_from = Polygon(150, 150, 100, 6)
+p_from = PolygonFactory.create_random(150, 150, 100, 5)
 p_from.paint(c)
 
-p_to = Polygon(650, 450, 100, 5)
+p_to = PolygonFactory.create_random(650, 450, 100, 6)
 p_to.paint(c)
 
 mainloop()
