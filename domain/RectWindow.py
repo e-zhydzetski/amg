3b0dd611579547bd6930e8324f558717e266cc18
e.zhydzetski <e.zhydzetski@gmail.com>
2017-09-13 17:32:21 +0300

    first implementation of koen-saserland algorithm

--- domain/RectWindow.py
+++ domain/RectWindow.py
@@ -16,3 +16,75 @@ class RectWindow:
 
     def paint(self, canvas):
         canvas.create_rectangle(self.left, self.top, self.right, self.bottom, fill="", outline="gray")
+
+    def __calculate_mark_for_point(self, p):
+        mark = 0
+        if p.x < self.left:
+            mark |= 1
+        if p.x > self.right:
+            mark |= 2
+        if p.y > self.bottom:
+            mark |= 4
+        if p.y < self.top:
+            mark |= 8
+        return mark
+
+    @staticmethod
+    def __cut_with_vertical_border(x, p1, p2):
+        dp = p2 - p1
+        dx = x - p1.x
+        return domain.Point(x, p1.y + dp.y * (dx / dp.x)), p2
+
+    @staticmethod
+    def __cut_with_horizontal_border(y, p1, p2):
+        dp = p2 - p1
+        dy = y - p1.y
+        return domain.Point(p1.x + dp.x * (dy / dp.y), y), p2
+
+    def cut_segment(self, p1, p2):
+        mark1 = self.__calculate_mark_for_point(p1)
+        mark2 = self.__calculate_mark_for_point(p2)
+
+        if mark1 | mark2 == 0:
+            return p1, p2
+
+        if mark1 & mark2:
+            return None, None
+
+        mark_diff = mark1 ^ mark2
+
+        if mark_diff & 1:
+            if mark1 & 1:
+                p1, p2 = self.__cut_with_vertical_border(self.left, p1, p2)
+                mark1 = self.__calculate_mark_for_point(p1)
+            else:
+                p2, p1 = self.__cut_with_vertical_border(self.left, p2, p1)
+                mark2 = self.__calculate_mark_for_point(p2)
+            mark_diff = mark1 ^ mark2
+
+        if mark_diff & 2:
+            if mark1 & 2:
+                p1, p2 = self.__cut_with_vertical_border(self.right, p1, p2)
+                mark1 = self.__calculate_mark_for_point(p1)
+            else:
+                p2, p1 = self.__cut_with_vertical_border(self.right, p2, p1)
+                mark2 = self.__calculate_mark_for_point(p2)
+            mark_diff = mark1 ^ mark2
+
+        if mark_diff & 4:
+            if mark1 & 4:
+                p1, p2 = self.__cut_with_horizontal_border(self.bottom, p1, p2)
+                mark1 = self.__calculate_mark_for_point(p1)
+            else:
+                p2, p1 = self.__cut_with_horizontal_border(self.bottom, p2, p1)
+                mark2 = self.__calculate_mark_for_point(p2)
+            mark_diff = mark1 ^ mark2
+
+        if mark_diff & 8:
+            if mark1 & 8:
+                p1, p2 = self.__cut_with_horizontal_border(self.top, p1, p2)
+            else:
+                p2, p1 = self.__cut_with_horizontal_border(self.top, p2, p1)
+
+        return p1, p2
+
--- task_1.py
+++ task_1.py
@@ -15,7 +15,7 @@ route = domain.Route(p_from, p_to, 200)
 
 # route.paint(canvas)
 
-prev_polygon = None
+cur_polygon = None
 
 shadow_mode = False
 
@@ -24,21 +24,27 @@ window_mode = True
 if window_mode:
     window = domain.RectWindow(domain.Point(300, 300), domain.Point(600, 600))
     window.paint(canvas)
+    p1 = domain.Point(320, 700)
+    p2 = domain.Point(200, 200)
+    canvas.create_line(p1.x, p1.y, p2.x, p2.y)
+    p1, p2 = window.cut_segment(p1, p2)
+    if p1 is not None:
+        canvas.create_line(p1.x, p1.y, p2.x, p2.y, fill='red')
 
 
 def paint_move():
     global route
     global canvas
-    global prev_polygon
+    global cur_polygon
     global shadow_mode
 
     if not route.finished():
-        if prev_polygon is not None and not shadow_mode:
-            prev_polygon.clear()
-        prev_polygon = route.next_step()
-        prev_polygon.paint(canvas)
+        if cur_polygon is not None and not shadow_mode:
+            cur_polygon.clear()
+        cur_polygon = route.next_step()
+        cur_polygon.paint(canvas)
         canvas.after(20, paint_move)
 
 
-canvas.after(500, paint_move)
+# canvas.after(500, paint_move)
 mainloop()
