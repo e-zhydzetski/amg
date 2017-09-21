a6676770b58bac119d7f79788eec6ad396c37d91
e.zhydzetski <e.zhydzetski@gmail.com>
2017-09-21 16:20:52 +0300

    add revert lifehack

--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
-shadow_mode = False
-window_mode = 2
+shadow_mode = True
+window_mode = 1
+revert = False
 
 visible_background_color = 'white'
 invisible_background_color = 'lightblue'
@@ -9,3 +10,8 @@ visible_border_color = ''
 
 invisible_color = ''
 invisible_border_color = 'gray'
+
+if revert:
+    visible_background_color, invisible_background_color = invisible_background_color, visible_background_color
+    invisible_color = visible_color
+    visible_color = visible_background_color
